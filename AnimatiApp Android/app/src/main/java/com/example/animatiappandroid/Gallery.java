2d60fd73efc4d40299b63d4d5984ecf2591dea14
Dali Palomeque <dalilapalomeque@mi.unc.edu.ar>
2024-09-18 14:32:03 -0300

    Incorporación de gallery

---
+++ AnimatiApp Android/app/src/main/java/com/example/animatiappandroid/Gallery.java
@@ -0,0 +1,27 @@
+package com.example.animatiappandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Gallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        setContentView(R.layout.activity_gallery);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
