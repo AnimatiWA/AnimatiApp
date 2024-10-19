8c5c86c298edd76597aec26e97429474aa30b1b5
Uriel Agustin Rochetti <urochetti@municipalidad.com>
2024-10-19 00:45:51 -0300

    Config

--- AnimatiApp Django/AnimatiBack/AnimatiCrud/settings.py
+++ AnimatiApp Django/AnimatiBack/AnimatiCrud/settings.py
@@ -150,13 +150,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-
-
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/imagenes/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/imagenes')
