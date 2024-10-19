344631035363302571466da138a7bb4a5d258b24
Uriel Agustin Rochetti <urochetti@municipalidad.com>
2024-10-18 22:45:07 -0300

    Se quita mysqlclient

--- AnimatiApp Django/AnimatiBack/AnimatiCrud/settings.py
+++ AnimatiApp Django/AnimatiBack/AnimatiCrud/settings.py
@@ -14,6 +14,8 @@ from decouple import config
 from datetime import timedelta
 from pathlib import Path
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
