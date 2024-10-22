afdc8263b825d103eb132b1f4d2eb21069ba3da3
Dali Palomeque <dalilapalomeque@mi.unc.edu.ar>
2024-10-22 17:04:52 -0300

    Trabajo en vista de Gallery

--- AnimatiApp Android/app/src/main/java/com/example/animatiappandroid/Gallery.java
+++ AnimatiApp Android/app/src/main/java/com/example/animatiappandroid/Gallery.java
@@ -1,12 +1,18 @@
 package com.example.animatiappandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import com.example.animatiappandroid.R;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class Gallery extends AppCompatActivity {
 
@@ -23,5 +29,15 @@ public class Gallery extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void op_carrito(View view) {
+        Intent intent = new Intent(this, CartActivity.class);
+        startActivity(intent);
+    }
+
+    public void op_menu(View view) {
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
     }
 }
