3bb49db4884d5e8fd95b69d78a9b560553ee5ff5
Dali Palomeque <dalilapalomeque@mi.unc.edu.ar>
2024-10-23 00:43:21 -0300

    Crea Layout Recycler para hacer las tarjetas de los productos en la galeria

--- AnimatiApp Android/app/src/main/java/com/example/animatiappandroid/Gallery.java
+++ AnimatiApp Android/app/src/main/java/com/example/animatiappandroid/Gallery.java
@@ -13,8 +13,16 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.PopupMenu;
 import android.widget.Toast;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Gallery extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ProductAdapter productAdapter;
+    private List<Product> productList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,17 @@ public class Gallery extends AppCompatActivity {
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
 
         setContentView(R.layout.activity_gallery);
+
+        recyclerView=findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        productList=new ArrayList<>();
+        productList.add(new Product("Product 1", 10.99, 1));
+        productList.add(new Product("Product 2", 19.99, 1));
+        productList.add(new Product("Product 3", 29.99, 1));
+
+        productAdapter=new ProductAdapter(productList,this);
+        recyclerView.setAdapter(productAdapter);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -40,4 +59,11 @@ public class Gallery extends AppCompatActivity {
         Intent intent = new Intent(this, MainMenuActivity.class);
         startActivity(intent);
     }
+
+    public void viewCart(View view) {
+        Intent intent = new Intent(this, CartActivity.class);
+        startActivity(intent);
+    }
+
+
 }
