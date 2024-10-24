09196cb2ca9783eecc3bcfd9ddac82b70f8bdce0
Dali Palomeque <dalilapalomeque@mi.unc.edu.ar>
2024-10-24 00:25:11 -0300

    GModificaciones en galería

--- AnimatiApp Android/app/src/main/java/com/example/animatiappandroid/Gallery.java
+++ AnimatiApp Android/app/src/main/java/com/example/animatiappandroid/Gallery.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.animatiappandroid.R;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -12,6 +14,7 @@ import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.core.content.ContextCompat;
+import android.content.Context;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.PopupMenu;
@@ -20,12 +23,37 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Gallery extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private ProductAdapter productAdapter;
     private List<Product> productList;
+    private SharedPreferences sharedPreferences;
+    private RequestQueue requestQueue;
+    private String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +70,12 @@ public class Gallery extends AppCompatActivity {
                 DividerItemDecoration.VERTICAL);
         dividerItemDecoration.setDrawable(ContextCompat.getDrawable(this, R.drawable.item_divider));
         recyclerView.addItemDecoration(dividerItemDecoration);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        sharedPreferences=getSharedPreferences("AnimatiPreferencias", Context.MODE_PRIVATE);
+
+        token=sharedPreferences.getString("token","");
 
         cargarProductos();
 
@@ -69,13 +103,167 @@ public class Gallery extends AppCompatActivity {
 
     public void cargarProductos(){
 
-
-        productList.add(new Product(nombre, precio, cantidad));
         productList = new ArrayList<>();
 
-        productAdapter=new ProductAdapter(productList,this);
-        recyclerView.setAdapter(productAdapter);
+        String token = sharedPreferences.getString("token", "");
 
-        productAdapter.notifyDataSetChanged();
+        String url = "https://animatiapp.up.railway.app/api/producto/lista";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject productObject = response.getJSONObject(i);
+                                int id = productObject.getInt("Codigo_Producto");
+                                String nombre = productObject.getString("Nombre_Producto");
+                                double precio = productObject.getDouble("Precio");
+                                int cantidad = 1;
+
+                                productList.add(new Product(id, nombre, precio, cantidad));
+                            }
+
+                            productAdapter = new ProductAdapter(productList, Gallery.this);
+                            recyclerView.setAdapter(productAdapter);
+                            productAdapter.notifyDataSetChanged();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(Gallery.this, "Error procesando los datos", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Gallery.this, "Error en la petición: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.d("Gallery","Cargar producto fallo");
+                    }
+
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    public void agregarAlCarrito(Product product) {
+        sharedPreferences = getSharedPreferences("AnimatiPreferencias", Context.MODE_PRIVATE);
+        int idCarrito = sharedPreferences.getInt("idCarrito", -1);
+
+        if (idCarrito == -1) {
+            crearYAgregar(product);
+        }
+        else {
+            agregar(product);
+        }
+    }
+
+    public void crearYAgregar(Product product) {
+        String url = "https://animatiapp.up.railway.app/api/carrito/crear";
+        int idUsuario= sharedPreferences.getInt("idUsuario",-1);
+        JSONObject jsonObject= new JSONObject();
+        try{
+            jsonObject.put("Usuario",idUsuario);
+        }
+        catch(JSONException e){
+            e.printStackTrace();
+        }
+
+        Log.d("GalleryId", ""+idUsuario);
+        JsonObjectRequest jsonObjectRequest= new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                jsonObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            int idCarrito = response.getInt("id");
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putInt("idCarrito", idCarrito);
+                            editor.apply();
+                            agregar(product);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                },new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Gallery.this, "Error en la petición: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d("Gallery","Cargar y agregar fallo");
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    public void agregar(Product product) {
+        int idCarrito = sharedPreferences.getInt("idCarrito", -1);
+        int codigoProducto = product.getId();
+        int cantidad = 1; // Cantidad por defecto
+
+
+        if (idCarrito == -1) {
+            Toast.makeText(this, "Carrito no válido", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String url = "https://animatiapp.up.railway.app/api/carritoProductos/crear";
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("Codigo", codigoProducto);
+            jsonObject.put("Carrito", idCarrito);
+            jsonObject.put("Cantidad", cantidad);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                jsonObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Toast.makeText(Gallery.this, "Producto agregado al carrito", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(Gallery.this, "Error, no se puedo agregar el producto al carrito.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+
+        requestQueue.add(jsonObjectRequest);
     }
 }
