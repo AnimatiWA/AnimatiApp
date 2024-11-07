4e4ed916ff5f69c2022681adcc5f683da38ccd40
Luciana Ferace <feraceuzi@gmail.com>
2024-11-07 01:03:11 -0300

    mejoras del layout en recuperacion de contraseña y conexion con el back

--- AnimatiApp Android/app/src/main/java/com/example/animatiappandroid/ForgotPassEmailActivity.java
+++ AnimatiApp Android/app/src/main/java/com/example/animatiappandroid/ForgotPassEmailActivity.java
@@ -9,36 +9,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.regex.Pattern;
 import org.json.JSONObject;
 
 public class ForgotPassEmailActivity extends AppCompatActivity {
     private Button sendButton;
-    private EditText newPasswordEditText, repeatPasswordEditText;
+    private EditText newPasswordEditText, repeatPasswordEditText, codeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_pass_email);
 
-        // Inicialización de las vistas
+
         sendButton = findViewById(R.id.buttonSend);
+        codeEditText = findViewById(R.id.codeEditText);
         newPasswordEditText = findViewById(R.id.newPasswordEditText);
         repeatPasswordEditText = findViewById(R.id.repeatPasswordEditText);
 
-        // Manejar el deep link
+
         Intent intent = getIntent();
         if (Intent.ACTION_VIEW.equals(intent.getAction())) {
             Uri uri = intent.getData();
             if (uri != null) {
-                // Extraer el token del enlace
+
                 String token = uri.getLastPathSegment();
-                // Guardar el token en SharedPreferences para su uso posterior
+
                 SharedPreferences sharedPreferences = getSharedPreferences("AnimatiPreferencias", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString("token", token);
@@ -46,17 +47,17 @@ public class ForgotPassEmailActivity extends AppCompatActivity {
             }
         }
 
-        // Listener para el botón "Enviar"
+
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Obtener las contraseñas introducidas por el usuario
+
                 String newPassword = newPasswordEditText.getText().toString();
                 String repeatPassword = repeatPasswordEditText.getText().toString();
 
-                // Validar que las contraseñas coincidan y cumplan con los requisitos
+
                 if (validatePasswords(newPassword, repeatPassword)) {
-                    // Ejecutar la tarea de recuperación de contraseña
+
                     new RecoverPasswordTask(newPassword).execute();
                 }
             }
@@ -64,13 +65,13 @@ public class ForgotPassEmailActivity extends AppCompatActivity {
     }
 
     private boolean validatePasswords(String newPassword, String repeatPassword) {
-        // Verificar si las contraseñas no coinciden
+
         if (!newPassword.equals(repeatPassword)) {
             showAlertDialog("Error", "Las contraseñas no coinciden.");
             return false;
         }
 
-        // Validar que la contraseña cumpla con los requisitos
+
         if (!validatePassword(newPassword)) {
             showAlertDialog("Error", "La contraseña debe tener al menos 6 caracteres, una letra mayúscula, una letra minúscula y un número.");
             return false;
@@ -80,13 +81,12 @@ public class ForgotPassEmailActivity extends AppCompatActivity {
     }
 
     private boolean validatePassword(String password) {
-        // Patrón para validar la contraseña
-        Pattern passwordPattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{6,}$");
-        return passwordPattern.matcher(password).matches();
+
+        return password.length() >= 6;
     }
 
     private void showAlertDialog(String title, String message) {
-        // Mostrar un cuadro de diálogo de alerta
+
         new AlertDialog.Builder(this)
                 .setTitle(title)
                 .setMessage(message)
@@ -95,7 +95,7 @@ public class ForgotPassEmailActivity extends AppCompatActivity {
     }
 
     private void showSuccessDialog() {
-        // Mostrar un cuadro de diálogo de éxito
+
         new AlertDialog.Builder(this)
                 .setTitle("Éxito")
                 .setMessage("Contraseña actualizada exitosamente.")
@@ -113,32 +113,36 @@ public class ForgotPassEmailActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(Void... voids) {
             try {
-                // Obtener el token desde SharedPreferences
-                SharedPreferences sharedPreferences = getSharedPreferences("AnimatiPreferencias", Context.MODE_PRIVATE);
-                String token = sharedPreferences.getString("token", "");
 
-                // Crear la URL para la solicitud al backend de Django
-                URL url = new URL("https://animatiapp.up.railway.app/reset-password/" + token);
+                SharedPreferences sharedPreferences = getSharedPreferences("AnimatiPreferencias", Context.MODE_PRIVATE);
+                String codigo = codeEditText.getText().toString().trim();
+
+
+                URL url = new URL("https://animatiapp.up.railway.app/api/resetPassword");
 
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("POST");
                 urlConnection.setRequestProperty("Content-Type", "application/json");
                 urlConnection.setDoOutput(true);
 
-                // Crear el objeto JSON con los datos de la contraseña
-                JSONObject jsonParam = new JSONObject();
-                jsonParam.put("password", newPassword);
-                jsonParam.put("password2", newPassword);  // Asegurarse de que ambas contraseñas se envíen
 
-                // Enviar los datos
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("codigo", codigo);
+                jsonParam.put("password", newPassword);
+                jsonParam.put("password2", newPassword);
+
+
                 OutputStream os = urlConnection.getOutputStream();
                 os.write(jsonParam.toString().getBytes("UTF-8"));
                 os.close();
 
-                // Obtener la respuesta del servidor
+
                 int responseCode = urlConnection.getResponseCode();
                 if (responseCode == HttpURLConnection.HTTP_OK) {
                     return "Contraseña actualizada con éxito";
+                } else if (responseCode == HttpURLConnection.HTTP_BAD_REQUEST) {
+
+                    return "Código incorrecto. Por favor, verifica el código de verificación.";
                 } else {
                     return "Error en la actualización de la contraseña: " + responseCode;
                 }
@@ -154,7 +158,12 @@ public class ForgotPassEmailActivity extends AppCompatActivity {
             if (result.equals("Contraseña actualizada con éxito")) {
                 showSuccessDialog();
             } else {
-                showAlertDialog("Error", result);
+
+                if (result.contains("Código incorrecto")) {
+                    Toast.makeText(ForgotPassEmailActivity.this, "El código de verificación es incorrecto. Por favor, inténtalo de nuevo.", Toast.LENGTH_LONG).show();
+                } else {
+                    showAlertDialog("Error", result);
+                }
             }
         }
     }
