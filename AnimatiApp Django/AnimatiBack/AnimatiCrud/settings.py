9403c4ebe2dad37a6848a8881101c61c10dda534
RochettiUriel <urielrochetti@gmail.com>
2025-07-25 15:44:33 -0300

    Se cambia config de Django

--- AnimatiApp Django/AnimatiBack/AnimatiCrud/settings.py
+++ AnimatiApp Django/AnimatiBack/AnimatiCrud/settings.py
@@ -79,9 +79,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication',],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
         'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.IsAdminUser',
     ],
     
 }
